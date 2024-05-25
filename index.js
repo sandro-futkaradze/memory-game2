22171c17d9a057ec60fb7159e7c57d9789d6c2c6
sandro-futkaradze <159762608+sandro-futkaradze@users.noreply.github.com>
2024-05-25 23:29:08 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
-let nameInput = document.querySelector(".name-input")
-let btn = document.querySelector(".submit-btn")
-let name = document.querySelector(".name")
-let gender = document.querySelector(".gender")
-let probability = document.querySelector(".probability")
-let count = document.querySelector(".count")
-let msgTxt = document.querySelector(".msg-txt")
+let clickedCard = null;
+let preventClick = false;
+let combosFound = 0;
 
-let getGender = async () => {
-    try {
-        let inputTxt = nameInput.value.trim()
-        let response = await fetch(`https://api.genderize.io?name=${inputTxt}`)
-        let data = await response.json()
-        if(data.gender === null || data.gender === undefined) {
-            msgTxt.innerHTML = "<h2>Please enter a valid name...</h2>"
-            msgTxt.style.color = "red"
-        }
-        else {
-            name.innerHTML = `<h3>Name: ${data.name}</h1>`
-            gender.innerHTML = `<h3>Gender: ${data.gender}</h1>`
-            probability.innerHTML = `<h3>Probability: ${data.probability}</h1>`
-            count.innerHTML = `<h3>Count: ${data.count}</h1>`
-            msgTxt.innerHTML = "<h4>Hope I detected the gender correctly...</h4>"
-            msgTxt.style.color = "green"
-        }
-    } catch (error) {
-        console.log(`${error}`)
-    }
+const colors = [
+  'pink',
+  'yellow',
+  'red',
+  'cyan',
+  'blue',
+  'teal',
+  'orange',
+  'green'
+];
+
+const cards = [...document.querySelectorAll('.card')];
+for (let color of colors) {
+  const cardAIndex = parseInt(Math.random() * cards.length);
+  const cardA = cards[cardAIndex];
+  cards.splice(cardAIndex, 1);
+  cardA.className += ` ${color}`;
+  cardA.setAttribute('data-color', color);
+
+  const cardBIndex = parseInt(Math.random() * cards.length);
+  const cardB = cards[cardBIndex];
+  cards.splice(cardBIndex, 1);
+  cardB.className += ` ${color}`;
+  cardB.setAttribute('data-color', color);
 }
-btn.addEventListener("click", getGender)
+
+
+function onCardClicked(e) {
+  const target = e.currentTarget;
+
+  if (
+    preventClick ||
+    target === clickedCard ||
+    target.className.includes('done')
+  ) {
+    return;
+  }
+
+  target.className = target.className
+    .replace('color-hidden', '')
+    .trim();
+  target.className += ' done';
+
+  if (!clickedCard) {
+    clickedCard = target;
+  } else if (clickedCard) {
+    if (
+      clickedCard.getAttribute('data-color') !==
+      target.getAttribute('data-color')
+    ) {
+      preventClick = true;
+      setTimeout(() => {
+        clickedCard.className =
+          clickedCard.className.replace('done', '').trim() +
+          ' color-hidden';
+        target.className =
+          target.className.replace('done', '').trim() +
+          ' color-hidden';
+        clickedCard = null;
+        preventClick = false;
+      }, 500);
+    } else {
+      combosFound++;
+      clickedCard = null;
+      if (combosFound === 8) {
+        alert('YOU WIN');
+      }
+    }
+  }
+}
